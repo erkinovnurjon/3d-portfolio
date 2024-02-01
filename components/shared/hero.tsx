88731c413a3b1a6ef789cc71cb0478898f2f9f98
Nurjon <nurjon2477@gmail.com>
2024-02-01 16:17:50 +0500

    end navbar

---
+++ components/shared/hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="felx justify-between items-center">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Hero;
--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,17 +1,31 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import Logo from "./logo";
+import { useScrolled } from "@/hooks/use-scrolled";
 
 const Navbar = () => {
+  const scrolled = useScrolled();
   return (
     <>
-      <div className="container h-[10vh] py-3 w-full mx-auto">
-        <div className="flex justify-between items-center">
-           <Logo />
-           <div className="flex items-center gap-x-2">
-            <Button variant={"ghost"} size={"sm"}>Modeling</Button>
-            <Button variant={"ghost"} size={"sm"}>Design</Button>
-           </div>
+      <div
+        className={cn(
+          "z-50 bg-background container mx-auto fixed top-0 flex items-center w-full py-6 px-8 justify-between",
+          scrolled && "border-b shadow-sm"
+        )}
+      >
+        <div className="flex justify-center items-center">
+          <Logo />
         </div>
+          <div className="flex items-center gap-x-2">
+            <Button variant={"ghost"} size={"sm"}>
+              Modeling
+            </Button>
+            <Button variant={"ghost"} size={"sm"}>
+              Design
+            </Button>
+          </div>
       </div>
     </>
   );
